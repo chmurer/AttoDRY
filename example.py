56abcc06ddbdcd798445de30be18e5289ef5958a
Christoph Murer <chmurer@gmail.com>
2020-10-14 09:44:19 +0200

    Add files via upload
    Very simple example code connecting to the AttoDRY on COMPort 4 (check with the LabView Interface which COMPort you are using). Reads out the field and temperature, sets a field and temperature and toggles the field and temperature control. Disconnects and ends the program. Check at the AttoDRY display if the values are changed accordingly and the field and temperature control are toggled (orange instead of white on the display).

---
+++ example.py
@@ -0,0 +1,44 @@
+from PyAttoDRY2100 import AttoDRY
+import time
+
+print('Connect to the AttoDRY')
+AttoDRY.begin(setup_version=1)
+AttoDRY.Connect(COMPort='COM4')
+
+# you need to wait for initialization; if you just start sending
+# commands, the connection will be lost.
+time.sleep(10.0)
+
+IN = AttoDRY.isDeviceInitialised()
+CN = AttoDRY.isDeviceConnected()
+# state that it is initialized and connected:
+if IN==1 and CN ==1:
+	print('The AttoDRY device is initialized and connected')
+else:
+	print('something went wrong.')
+
+
+B = AttoDRY.getMagneticField()
+T = AttoDRY.getSampleTemperature()
+
+print('The current magnetic field is: '+str(B)+' T')
+print('The current temeperature is: '+str(T)+' K')
+
+time.sleep(1.0)
+
+AttoDRY.setUserMagneticField(0.01)
+AttoDRY.setUserTemperature(1.9)
+
+print('Both magnetic field and temperature have been set to new values. This will not change anything as long as field and temperature control are not toggled.')
+
+time.sleep(2.0)
+
+AttoDRY.toggleMagneticFieldControl()
+time.sleep(0.1)
+AttoDRY.toggleFullTemperatureControl()
+print('toggled Temperature and field control.')
+
+
+# always end by disconnect and end
+AttoDRY.Disconnect()
+AttoDRY.end()
